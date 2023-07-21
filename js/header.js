54641a8e95d0a047cb915d609adedffb812ca56b
Enayat Badru <Enayatbadru07@gmail.com>
2023-07-21 16:36:51 +0200

    This commit adds a sticky header with a dark background and white text, and a transition effect for the logo and text. It also adds a transition effect for the menu when it becomes sticky. Finally, it adds a transition effect for the button when it is hovered and clicked.

--- js/header.js
+++ js/header.js
@@ -1,14 +1,17 @@
 const header = document.querySelector("#header");
 const btn = document.querySelector(".btn-mobile")
-const body = document.querySelector("body")
+const menu = document.querySelector("#menu");
+const body = document.querySelector("body");
 
 // SCROLL
 window.addEventListener("scroll", (e)=>{
     console.log(window.scrollY);
+    header.classList.toggle('sticky', window.scrollY > 0)
 })
 
 // btn-mobile
 btn.addEventListener('click', ()=>{
     header.classList.toggle('active');
+    menu.classList.toggle('active');
 });
 
