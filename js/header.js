8fbeb1ec87b0289beb427b2a05de9cbd24ed330b
Enayat Badru <Enayatbadru07@gmail.com>
2023-07-23 00:09:54 +0200

    add-img, css and html, js

--- js/header.js
+++ js/header.js
@@ -1,11 +1,9 @@
 const header = document.querySelector("#header");
 const btn = document.querySelector(".btn-mobile")
 const menu = document.querySelector("#menu");
-const body = document.querySelector("body");
 
 // SCROLL
 window.addEventListener("scroll", (e)=>{
-    console.log(window.scrollY);
     header.classList.toggle('sticky', window.scrollY > 0)
 })
 
