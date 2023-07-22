9e4bb0cd3a2f02c0b5c7695c30a14baff2275cf7
Enayat Badru <Enayatbadru07@gmail.com>
2023-07-23 00:32:32 +0200

    Add mobile menu functionality to header.js

--- js/header.js
+++ js/header.js
@@ -1,15 +1,17 @@
 const header = document.querySelector("#header");
 const btn = document.querySelector(".btn-mobile")
 const menu = document.querySelector("#menu");
+const link = document.querySelector("#menu > a");
 
 // SCROLL
 window.addEventListener("scroll", (e)=>{
     header.classList.toggle('sticky', window.scrollY > 0)
 })
 
-// btn-mobile
-btn.addEventListener('click', ()=>{
+function ativar(){
     header.classList.toggle('active');
     menu.classList.toggle('active');
-});
+}
 
+btn.addEventListener('click', ativar);
+menu.addEventListener('click', ativar);
